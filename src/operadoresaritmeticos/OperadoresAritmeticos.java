b32941a725e51ff299b4484a6ad145bcfd3e62d0
Marieli Ficagna <marieli.ficagna@gmail.com>
2023-02-15 17:27:44 -0300

    first commit

---
+++ src/operadoresaritmeticos/OperadoresAritmeticos.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package operadoresaritmeticos;
+
+/**
+ *
+ * @author Mari (48) 99618-5728
+ */
+public class OperadoresAritmeticos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        /*int n1 = 5;
+        int n2 = 3;
+        float m = (n1+n2)/2;
+        System.out.println("A media é " + m);*/
+        
+        /*int numero = 10;
+        int valor = 4 + numero++;
+        System.out.println(valor);*/
+        
+        /*int x = 5;
+        x += 2; //x = 5+2
+        System.out.println(x);*/
+        
+        /*float y = 8.3f;
+        int ar = (int) Math.abs(y);
+        System.out.println(ar);*/
+        
+        double ale = Math.random();
+        int n = (int) (0 + ale * (100-0));
+        System.out.println(n);
+        
+        
+               
+               
+               
+           
+    }
+    
+}
